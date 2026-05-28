5ee81f5a06e845bc74e6dee08701e42e4d88bb06
Neha <your@email.com>
2026-05-28 21:06:33 +0530

    Initial Rust beginner project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod variables;
+mod functions;
+mod loops;
+mod ownership;
+mod borrowing;
+mod structs;
+mod enums;
+mod vectors;
+mod strings;
+mod option_result;
+mod practice;
+
+fn main() {
+    println!("Rust Beginner Basics Repository");
+    
+    variables::run();
+    structs::run();
+}
---
+++ src/variables.rs
@@ -0,0 +1,9 @@
+pub fn run() {
+    let x = 5;
+    let mut y = 10;
+
+    y += 1;
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+}
